b864fc34c7f1e21d602225f0946604f3698817f3
1marincoh2 <jose_aries93@hotmail.com>
2020-05-26 12:42:24 -0500

    realizando listas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Nombre from './Componentes/Nombresperro';
-import Nombre1 from './Componentes/Nombre1';
-import Nombre2 from './Componentes/Nombre2';
-import Nombre3 from './Componentes/Nombre3';
-import Nombre4 from './Componentes/Nombre4';
 
-import Mascota from './Componentes/Mascota';
-import Animal from './Componentes/Animal';
+
+
 
 function App() {
 	const pests = [
@@ -18,15 +12,17 @@ function App() {
 	]
 	return (
 		<div className="App">
-
-			<Animal name={'cookie'} species={'Perro'}></Animal>
-			<br/>
-			<Mascota name={'cookie'} species={'Perro'}></Mascota>
-			<br/>
 			{
+            	
 				pests.map((pet) => (
 					<div>
-						<Animal name={pet.n} species={pet.r}></Animal>
+						
+						
+						
+                         <ol>{pet.r}</ol>
+                        
+                         
+                        
 						<br/>
 					</div>
 				))
