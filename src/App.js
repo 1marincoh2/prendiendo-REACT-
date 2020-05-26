263eedf0d426bddfe0fc071832829062d172f56e
1marincoh2 <jose_aries93@hotmail.com>
2020-05-26 15:00:56 -0500

    realizando tablas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import Mascota from './Componentes/Mascota';
 
 function App() {
+
 	const pests = [
 		{n: 'animal', r: 'elefante'},
 		{n: 'animal', r: 'pajaro'},
@@ -11,6 +13,26 @@ function App() {
 	return (
 		<div className="App">
 			<ul>
+			
+			<table>
+             <thead>
+              <tr>
+               <th><Mascota></Mascota></th>
+			  </tr>
+             </thead>
+			 {
+			 pests.map((pet) =>(
+             <tbody>
+				 
+              <tr>
+               <td>{pet.n}</td>
+               <td>{pet.r}</td>
+              </tr>
+              
+             </tbody>
+			 ))
+			}
+            </table>
 				{
 					pests.map((pet) => (
 						<li>{pet.r}</li>
