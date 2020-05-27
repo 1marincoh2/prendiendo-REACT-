b3a6edbf1afec51e35dadd64d188f15319b536ae
MisaelMa <amisael.amir.misael@gmail.com>
2020-05-27 14:59:49 -0500

    se agrego el spiner

--- src/Componentes/List.jsx
+++ src/Componentes/List.jsx
@@ -2,33 +2,47 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap';
+
 
 function List() {
 	const [user, setUser] = useState([]);
+	const [loader, setLoader] = useState(false);
 
 	const getUser = () => {
+		setLoader(true)
 		axios.get('https://jsonplaceholder.typicode.com/users').then((reponse) => {
 			console.log(reponse.data)
 			setUser(reponse.data)
+			setLoader(false)
+		}).catch(() => {
+			setLoader(false)
 		})
 	}
 
 	return (
 		<div>
-            <Button variant="primary" onClick={() => {getUser()}} >Obtener usuarios</Button>
-          	{
-				user.map((u)=>(
-                    <Card>        
-                     <Card.Body>
-                      <Card.Title>{u.name}</Card.Title>
-                      <Card.Subtitle>{u.id}</Card.Subtitle>
-                      <Card.Text>{u.username}</Card.Text>
-                      <Card.Text>{u.email}</Card.Text>
-                     </Card.Body>
-                    </Card>    
-                     	))
-        	} 
-			</div>
+			<Button variant="primary" onClick={() => {
+				getUser()
+			}}>Obtener usuarios</Button>
+
+			{
+				loader ? <Spinner animation="border" variant="success"/> : null
+			}
+
+			{
+				user.map((u) => (
+					<Card key={u.id}>
+						<Card.Body>
+							<Card.Title>{u.name}</Card.Title>
+							<Card.Subtitle>{u.id}</Card.Subtitle>
+							<Card.Text>{u.username}</Card.Text>
+							<Card.Text>{u.email}</Card.Text>
+						</Card.Body>
+					</Card>
+				))
+			}
+		</div>
 	)
 }
 
