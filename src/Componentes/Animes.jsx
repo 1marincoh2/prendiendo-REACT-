cfb84610ea8b48ce441eef86172e173d522ef9af
1marincoh2 <jose_aries93@hotmail.com>
2020-05-28 14:59:55 -0500

    realizando el map en animes.jsx

--- src/Componentes/Animes.jsx
+++ src/Componentes/Animes.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 
 function Animes() {
-    const [anime, setSerie] =useState({});
+    const [serie, setSerie] =useState({});
  
 
 
@@ -25,10 +26,29 @@ function Animes() {
             <Button variant="primary" onClick={() => {
 				getSeries()
 			}}>Serie Animada</Button>
+ 
+            {	    
+					<Card >
+						<Card.Body>
+							<Card.Title>{serie.request_has}</Card.Title>
+							<Card.Subtitle>{serie.request_cached}</Card.Subtitle>
+							<Card.Text>{serie.request_cache_expiry}</Card.Text>
+							<Card.Text>{serie.last_page}</Card.Text>
+						</Card.Body>
+					</Card>
+				
+			}
 
-			
-
-            
+{
+				serie.results.map((personaje) => (
+					<Card >                     
+                     <Card.Img variant="top" src="image_url" /> 
+					 <Card.Title>{personaje.title}</Card.Title>
+					<Card.Subtitle>{personaje.image_url}</Card.Subtitle>
+						
+	           		</Card>
+				))
+			}
 
 		</div>
 	);
