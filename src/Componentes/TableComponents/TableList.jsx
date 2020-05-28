91d941a1e563a925ea40b418ad4b5f0b2197abfc
1marincoh2 <jose_aries93@hotmail.com>
2020-05-28 18:58:44 -0500

    actualizando tablelist

--- src/Componentes/TableComponents/TableList.jsx
+++ src/Componentes/TableComponents/TableList.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
-
+import { Spinner } from 'react-bootstrap';
 
 function TableList() {
 	const [user, setUser] = useState([]);
-	
+	const [loader, setLoader] = useState(false);
 
 	const getUser = () => {
-		
+		setLoader(true)
 		axios.get('https://jsonplaceholder.typicode.com/users').then((reponse) => {
 			console.log(reponse.data)
 			setUser(reponse.data)
-			
+			setLoader(false)
+		}).catch(() => {
+			setLoader(false)
 		})
+		
 	}
 
 	return (
@@ -21,35 +24,43 @@ function TableList() {
 			<Button variant="primary" onClick={() => {
 				getUser()
 			}}>Obtener usuarios</Button>
-
+              
+            { 
+				loader ? <Spinner animation="border" variant="success"/> : null
+			}
 	              <style>{`
               table, th, td {
               border: 1px solid black;
               border-collapse: collapse;
               } `}</style>
 
-			{
-				user.map((u) => (
+	
+				
 				<table >
 					<thead>
 					<tr>
-                        <th>Id</th>
+                        
 						<th>Firstname</th>
 						<th>Username</th>
 						<th>Email</th>
 					</tr>
 					</thead>
+                    
 					<tbody>
+                    {
+                        user.map((u) => (
 					<tr>
-                        <td>{u.Id}</td>
+                        
 						<td>{u.name}</td>
 						<td>{u.username}</td>
 						<td>{u.email}</td>
-					</tr>	
+                    </tr>	
+                        ))
+                    }
 					</tbody>
 				</table>
-								))
-			}
+								
+			
 		</div>
 	)
 }
