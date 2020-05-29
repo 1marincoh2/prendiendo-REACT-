4bdc72558550efcf186c50b30096312bebaa07bc
MisaelMa <amisael.amir.misael@gmail.com>
2020-05-29 18:21:19 -0500

    se agrego los bordes de ejemplo

--- src/Componentes/GridComponentes/GridContenedor.jsx
+++ src/Componentes/GridComponentes/GridContenedor.jsx
@@ -6,20 +6,20 @@ import Col from 'react-bootstrap/Col';
 
 function GridContenedor() {
     return (
-  
-        
+
+
      <Container fluid>
 
   <Row >
-    <Col style={{backgroundColor: 'black'}}  xl={12}>largo</Col>
-    <Col style={{backgroundColor: 'red'}}  xl={12} xs={6}>largo</Col>
-    <Col style={{backgroundColor: 'blue'}} xl={6} xs={6}>mediano</Col>
-    <Col style={{backgroundColor: 'green'}} xl={6} sm={12}>mediano</Col>
+    <Col style={{border: '1px solid #D81B60'}}  xl={12}>largo</Col>
+    <Col style={{border: '1px solid #D81B60'}}  xl={12} xs={6}>largo</Col>
+    <Col style={{border: '1px solid #D81B60'}} xl={6} xs={6}>mediano</Col>
+    <Col style={{border: '1px solid #D81B60'}} xl={6} sm={12}>mediano</Col>
   </Row>
-  
+
    </Container>
 
-        
+
 
 	)
 }
--- src/Componentes/GridComponentes/GridStudents.jsx
+++ src/Componentes/GridComponentes/GridStudents.jsx
@@ -8,62 +8,58 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 
-
 function GridStudenst() {
-    const [alumno, setAlumno] =useState([]);
-    const [loader, setLoader] = useState(false);
+	const [alumno, setAlumno] = useState([]);
+	const [loader, setLoader] = useState(false);
 
-      
-        const getAlumno = () => {
-            setLoader(true)		
+
+	const getAlumno = () => {
+		setLoader(true)
 		axios.get('https://api-dev.muunyal.telweb.app/school/students').then((reponse) => {
 			console.log(reponse.data)
-            setAlumno(reponse.data)
-            setLoader(false)
+			setAlumno(reponse.data)
+			setLoader(false)
 		}).catch(() => {
 			setLoader(false)
-        
-        })
-        }	
-    
-	
-       
 
-    return (
-                
+		})
+	}
 
-            <Container fluid>
-               
-               <Button variant="primary" onClick={() => {
-				 getAlumno()
-			    }}>INFO ALUMNO</Button>
- 
-                 {      
-                  loader ? <Spinner animation="border" variant="success"/> : null
-                 } 
-             <Row >
-                    {
-                        alumno.map((estudiante) => (
-                        <Col  style={{backgroundColor: 'blue'}}  xl={4} xs={6}>{estudiante.matricula}
-                        <Image width={"100%"} src={estudiante.profilePicture}/>
-                        <Row >
-                        <Col  style={{backgroundColor: 'green'}}  xl={6} xs={3}>{estudiante.name}</Col>
-                        </Row>
-                        <Row >
-                        <Col  style={{backgroundColor: 'red'}}  xl={6} xs={3}>{estudiante.lastNameFather}</Col>
-                        </Row>
-                        <Row >
-                        <Col  style={{backgroundColor: 'brown'}}  xl={6} xs={3}>{estudiante.lastNameMother}</Col>                       
-                        </Row>     
-                        </Col>
 
-						
-                            
-                       ))
-                    }
-                </Row>
- 
-          </Container>
+	return (
+
+
+		<Container fluid>
+
+			<Button variant="primary" onClick={() => {
+				getAlumno()
+			}}>INFO ALUMNO</Button>
+
+			{
+				loader ? <Spinner animation="border" variant="success"/> : null
+			}
+			<Row>
+				{
+					alumno.map((estudiante) => (
+						<Col style={{backgroundColor: 'blue'}} xl={4} xs={6}>{estudiante.matricula}
+							<Image width={'100%'} src={estudiante.profilePicture}/>
+							<Row>
+								<Col style={{backgroundColor: 'green'}} xl={6} xs={3}>{estudiante.name}</Col>
+							</Row>
+							<Row>
+								<Col style={{backgroundColor: 'red'}} xl={6} xs={3}>{estudiante.lastNameFather}</Col>
+							</Row>
+							<Row>
+								<Col style={{backgroundColor: 'brown'}} xl={6} xs={3}>{estudiante.lastNameMother}</Col>
+							</Row>
+						</Col>
+
+
+					))
+				}
+			</Row>
+
+		</Container>
 
 	);
 }
