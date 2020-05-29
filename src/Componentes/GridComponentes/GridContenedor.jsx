8e1e91660d7314f2cd29ae2a3d0601934e665a2e
MisaelMa <amisael.amir.misael@gmail.com>
2020-05-29 18:27:21 -0500

    se agrego el color de fondo

--- src/Componentes/GridComponentes/GridContenedor.jsx
+++ src/Componentes/GridComponentes/GridContenedor.jsx
@@ -11,7 +11,7 @@ function GridContenedor() {
      <Container fluid>
 
   <Row >
-    <Col style={{border: '1px solid #D81B60'}}  xl={12}>largo</Col>
+    <Col style={{border: '1px solid #D81B60', backgroundColor: '#AB47BC'}}  xl={12}>largo</Col>
     <Col style={{border: '1px solid #D81B60'}}  xl={12} xs={6}>largo</Col>
     <Col style={{border: '1px solid #D81B60'}} xl={6} xs={6}>mediano</Col>
     <Col style={{border: '1px solid #D81B60'}} xl={6} sm={12}>mediano</Col>
