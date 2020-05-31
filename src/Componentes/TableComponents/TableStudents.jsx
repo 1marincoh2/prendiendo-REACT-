74cce91fe25de94c744e3d8d621ef07b2168ef0d
MisaelMa <amisael.amir.misael@gmail.com>
2020-05-30 21:40:26 -0500

    se agrego la validacion de los perfiles

--- src/Componentes/TableComponents/TableStudents.jsx
+++ src/Componentes/TableComponents/TableStudents.jsx
@@ -6,74 +6,71 @@ import { Image } from 'react-bootstrap';
 import { Spinner } from 'react-bootstrap';
 
 
-
 function TableStudenst() {
-    const [alumno, setAlumno] =useState([]);
-    const [loader, setLoader] = useState(false);
+	const [alumno, setAlumno] = useState([]);
+	const [loader, setLoader] = useState(false);
 
-      
-        const getAlumno = () => {
-            setLoader(true)		
+
+	const getAlumno = () => {
+		setLoader(true)
 		axios.get('https://api-dev.muunyal.telweb.app/school/students').then((reponse) => {
 			console.log(reponse.data)
-            setAlumno(reponse.data)
-            setLoader(false)
+			setAlumno(reponse.data)
+			setLoader(false)
 		}).catch(() => {
 			setLoader(false)
-        
-        })
-        }	
-    
-	
-       
 
-    return (
-        <div>
-            <Button variant="primary" onClick={() => {
+		})
+	}
+
+
+	return (
+		<div>
+			<Button variant="primary" onClick={() => {
 				getAlumno()
 			}}>INFO ALUMNO</Button>
- 
-            { 
+
+			{
 				loader ? <Spinner animation="border" variant="success"/> : null
-			}             
-						
-             <Table striped bordered hover variant="dark" responsive>
+			}
 
-					<thead>
-					<tr >
-                        
-						<th>#id</th>
-						<th >Matricula</th>
-						<th>Nombre</th>
-						<th>Primer Apellido</th>
-						<th>Segundo Apellido</th>
-						<th>Foto</th>
+			<Table striped bordered hover variant="dark" responsive>
 
-					</tr>
-					</thead>
-                    
-					<tbody>
-                    {
+				<thead>
+				<tr>
 
-                       alumno.map((estudiante) => (
-                     
-                                                              
-					<tr style={{ width: '10rem ' }}>
-                        <td>{estudiante.id}</td>
-                        <td>{estudiante.matricula}</td>
-                        <td>{estudiante.name}</td>
-                        <td>{estudiante.lastNameFather}</td>
-                        <td>{estudiante.lastNameMother}</td>
-                        { 
-                        loader ? <Image  src={ estudiante.profilePicture}  style={{ width: '5rem ' }}/> : null
-                    }
-                    </tr>	
-                        ))
-                    
-                    }
-					</tbody>
-				</Table>
-         </div>
+					<th>#id</th>
+					<th>Matricula</th>
+					<th>Nombre</th>
+					<th>Primer Apellido</th>
+					<th>Segundo Apellido</th>
+					<th>Foto</th>
+
+				</tr>
+				</thead>
+
+				<tbody>
+				{
+
+					alumno.map((estudiante) => (
+						estudiante.profilePicture !== null ?
+							// estudiante.profilePicture === null ?
+							<tr style={{width: '10rem '}}>
+								<td>{estudiante.id}</td>
+								<td>{estudiante.matricula}</td>
+								<td>{estudiante.name}</td>
+								<td>{estudiante.lastNameFather}</td>
+								<td>{estudiante.lastNameMother}</td>
+								<td>
+									<Image src={estudiante.profilePicture} style={{width: '5rem '}}/>
+								</td>
+							</tr> : null
+					))
+
+				}
+				</tbody>
+			</Table>
+		</div>
 	);
 }
 
