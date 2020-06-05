205fb84002930e215afa7b1fdbec2a4a9960b6be
MisaelMa <amisael.amir.misael@gmail.com>
2020-06-04 19:37:50 -0500

    se agrego la siguen opcion

--- src/Componentes/FormulariosComponents/FormularioSelect.jsx
+++ src/Componentes/FormulariosComponents/FormularioSelect.jsx
@@ -1,42 +1,50 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 
 
-const FormularioSelect= () => {
+const FormularioSelect = () => {
 
-  const [frutas, setFrutas] = useState('coco');
-  
+    const [frutas, setFrutas] = useState('coco');
+    const fruts = ['naranja', 'limon', 'coco', 'mango', 'uva']
+
+    const handleonChange = (event) => {
+        setFrutas(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
 
-const handleonChange = (event) => {
-  setFrutas(event.target.value);
-}
+    return (
+        <>
+            {frutas}
 
-const handleSubmit = (event) => {
-    event.preventDefault();
-}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    selecione tu preferido:
+                    <select value={frutas} onChange={handleonChange}>
+                        <option value="naranja">naranja</option>
+                        <option value="limon">Limon</option>
+                        <option value="coco">Coco</option>
+                        <option value="mango">Mango</option>
+                        <option value="uva">uva</option>
+                    </select>
+                </label>
+            <br/>
+                <label>
+                    selecione tu segundi preferido:
+                    <select value={frutas} onChange={handleonChange}>
+                        {fruts.map(fruts =>
+                            <option key={fruts} value={fruts}>{fruts}</option>
+                        )};
+                    </select>
+                </label>
+
+            </form>
 
 
-return (
-<>
-  {frutas}
-
-  <form onSubmit={handleSubmit}>
-    <label>
-      selecione tu preferido:
-        <select value={frutas} onChange={handleonChange}>
-        <option value="naranja">naranja</option>
-        <option value="limon">Limon</option>
-        <option value="coco">Coco</option>
-        <option value="mango">Mango</option>
-        <option value="uva">uva</option>
-      </select>
-    </label>
-  
-      </form>
-
-      <input type="file" />
-  </>
-);
+        </>
+    );
 
 }
 
