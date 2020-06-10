c0c26d26d35ef8c188df28d506852de3844782a9
1marincoh2 <jose_aries93@hotmail.com>
2020-06-10 14:47:09 -0500

    realiznado un array string

--- src/Componentes/ComponentPractica/practica1.jsx
+++ src/Componentes/ComponentPractica/practica1.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
-
+import { Card,Button } from 'react-bootstrap';
 
 function Practica1() {
 
     const list = ['casa', 'caro', 'tele', 'radio', 'ventilador', 'mesa', 'silla', 'baño'];
+ 
+   
+  
 
 
+
+    
+   
+       
     return (
-        <div>
-            {list.map((obj) =>
-                <li key={obj}>{obj}</li>
-            )}
-        </div>
-    )
+        <div >
+  
 
+<Card style={{ width: '18rem' }}>
+<Card.Body>
+<Card.Title>lista</Card.Title>
+
+{list.map((obj) =>
+<Card.Text key={obj}>
+    {obj}
+</Card.Text>
+)}
+</Card.Body>
+</Card>
+</div>
+);
 }
-
+   
 export default Practica1;
