7e0d3a1ebe28179d12fe619ec4fd0f2449aa5056
1marincoh2 <jose_aries93@hotmail.com>
2020-06-12 17:34:02 -0500

    manejando props

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Ejemplo from '../Componentes/ejemplo';
 import Practica1 from '../Componentes/ComponentPractica/practica1';
+import Mascota from '../Componentes/Mascota';
 
 function HomePage() {
 
     return (
         <div className="HomePage">
 				HomePage
+				<Mascota name="buldog" species="perro" ></Mascota>
 				<Ejemplo></Ejemplo>
 				<Practica1></Practica1>
+				
 		</div>
 	);
 }
