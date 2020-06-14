9e034bbcac026bc3c0371a641f941bbdef301a16
1marincoh2 <jose_aries93@hotmail.com>
2020-06-14 13:28:39 -0500

    actualizando animesconcatenacio

--- src/Componentes/ComponentPractica/Animesconcatenacion.jsx
+++ src/Componentes/ComponentPractica/Animesconcatenacion.jsx
@@ -1,24 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Button, Image, Container, Row, Col} from 'react-bootstrap';
-import {useSelector} from "react-redux";
-import {useDispatch} from "react-redux";
+import { Button, Image, Container, Row, Col } from 'react-bootstrap';
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 
 const Animesconcatenacion = () => {
     const [anime, setAnime] = useState('');
     const [animes1, setAnimes1] = useState([]);
-    const [serie, setSerie] = useState({results: []});
+    const [serie, setSerie] = useState({ results: [] });
 
     const textSearch = useSelector(state => state);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        console.log('a cambiado la informacion')
-    },[textSearch])
-    const getSeries = () => {
+        getSeries(textSearch.text)
+    }, [textSearch])
 
-        axios.get('https://api.jikan.moe/v3/search/anime?q=' + anime).then((reponse) => {
+    const getSeries = (caricatura) => {
+
+        axios.get('https://api.jikan.moe/v3/search/anime?q=' + caricatura).then((reponse) => {
             console.log(reponse.data)
             setSerie(reponse.data)
 
@@ -48,20 +49,12 @@ const Animesconcatenacion = () => {
         <>
             <Container fluid>
 
-                <h1>{textSearch.text}</h1>
-
-                <form onSubmit={handlebuscar}>
-                    <input type="text" value={anime} onChange={handleChange}/>
-                    <Button variant="primary" onClick={() => {
-                        getSeries()
-                    }}>buscar</Button>
-                </form>
 
                 <Row>
                     {
                         serie.results.map((personaje) => (
-                            <Col style={{backgroundColor: '#AB47BC'}} xl={2} xs={6}>{personaje.title}
-                                <Image style={{border: '1px solid #D81B60'}} width={"100%"} src={personaje.image_url}/></Col>
+                            <Col style={{ backgroundColor: '#AB47BC' }} xl={2} xs={6}>{personaje.title}
+                                <Image style={{ border: '1px solid #D81B60' }} width={"100%"} src={personaje.image_url} /></Col>
 
 
                         ))
